d12d6d1345246d780d3856d437e68e2a78e5afb2
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2022-11-02 00:50:34 +0100

    Add test in functions
    Add test to functions remplirReservoir, Deverser to check if it has the right to do it

--- tpl_2A_POO/codeEtudiants/src/Robot/Chenille.java
+++ tpl_2A_POO/codeEtudiants/src/Robot/Chenille.java
@@ -36,6 +36,10 @@ public class Chenilles
             if (vitesse > 80) {
                 throw new VitesseIncorrectException("La vitesse ne peut pas être supérieure à 80km/h");
             }
+            /* Si la vitesse n'a pas été spécifiée, la mettre par défaut */
+            if (Double.isNaN(vitesse)) {
+                vitesse = 80;
+            }
         } catch (VitesseIncorrectException e) {
             System.out.println(e.getMessage());
         }
@@ -79,22 +83,39 @@ public class Chenilles
             if (vol > this.volumeEau) {
                 throw new VolumeEauIncorrectException("Le volume d'eau ne peut pas être supérieur au volume d'eau restant");
             }
+            boolean peuxDeverser = false;
+            /* on regarde si la case est un incendie */
+            for (Incendie incendie : this.position.getIncendies()) {
+                if (incendie != null) {
+                    peuxDeverser = true;
+                }
+            }
+            if (peuxDeverser) {
+                this.volumeEau -= vol;
+            }
         } catch (VolumeEauIncorrectException e) {
             System.out.println(e.getMessage());
         }
-        this.volumeEau -= vol;
     }
 
     public void remplirReservoir()
     {
-        /* vérifie si une des cases alentours est de type eau */
-        for (int i = 0; i < 4; i++) {
-            if (this.position.getVoisin(i).getNature() == NatureTerrain.EAU) {
-                this.volumeEau = 1000;
-                return;
+        try {
+            /* variable si condition respectée ou non */
+            boolean peuRemplir = false;
+            /* vérifie si une des cases alentours est de type eau */
+            for (Direction dir : Direction.values()) {
+                if (getVoisin(this.position, dir).getNature() == NatureTerrain.EAU) {
+                    boolean condition = true;
+                }
             }
+            if (!peuRemplir) 
+            {
+                throw new NoSuchElementException("Il n'y a pas d'eau autour du robot");
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
         }
-        /* test */
         this.volumeEau = 2000;
     }
 }
