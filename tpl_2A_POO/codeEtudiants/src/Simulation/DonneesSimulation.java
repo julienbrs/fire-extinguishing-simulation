7f4b85ab4eaa96bb3dda0e53639059825f21047e
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2022-11-02 12:04:22 +0100

    fixing constructor prototype

--- tpl_2A_POO/codeEtudiants/src/Simulation/DonneesSimulation.java
+++ tpl_2A_POO/codeEtudiants/src/Simulation/DonneesSimulation.java
@@ -1,16 +1,18 @@
 package Simulation;
 import Carte.Carte;
+import Carte.Case;
 import Carte.Incendie;
 import Robot.Robot;
+import java.util.HashMap;
 // For now we don't need anything
 // Will have to check how to display 
 // In the following part of the subject
 public class DonneesSimulation
 {
     private Carte carte;
-    private Incendie[] incendies;
+    private HashMap<Case, Incendie> incendies;
     private Robot[] robots;
-    public DonneesSimulation(Carte carte, Incendie[] incendies, Robot[] robots)
+    public DonneesSimulation(Carte carte, HashMap<Case, Incendie> incendies, Robot[] robots)
     {
         this.carte = carte;
         this.incendies = incendies;
