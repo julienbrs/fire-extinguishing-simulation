c16c89ddbe3ddaa260d0d03db94864ce55b922b1
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2022-11-11 13:07:23 +0100

    removing vitesse exception on getVitesse

--- tpl_2A_POO/codeEtudiants/src/Robot/Chenilles.java
+++ tpl_2A_POO/codeEtudiants/src/Robot/Chenilles.java
@@ -5,12 +5,13 @@ import Exception.*;
 import Carte.*;
 import Simulation.DonneesSimulation;
 
-public class Chenilles extends Robot
-{   
-    static double vitesseDefaut =  60;
+public class Chenilles extends Robot {
+    static double vitesseDefaut = 60;
     static double vitesseMax = 80;
     static int volumeEauMax = 2000;
-    public Chenilles(Case position, int volumeEau, double vitesse, DonneesSimulation donnees) throws VitesseIncorrectException
+
+    public Chenilles(Case position, int volumeEau, double vitesse, DonneesSimulation donnees)
+            throws VitesseIncorrectException
 
     {
         super(position, volumeEau, vitesse, donnees);
@@ -19,7 +20,8 @@ public class Chenilles extends Robot
             throw new VitesseIncorrectException("La vitesse ne peut pas être négative");
         }
         if (vitesse > vitesseMax) {
-            throw new VitesseIncorrectException("La vitesse ne peut pas être supérieure à 80km/h pour un robot Chenilles");
+            throw new VitesseIncorrectException(
+                    "La vitesse ne peut pas être supérieure à 80km/h pour un robot Chenilles");
         }
         /* Si la vitesse n'a pas été spécifiée, la mettre par défaut */
         if (Double.isNaN(vitesse)) {
@@ -27,35 +29,42 @@ public class Chenilles extends Robot
         }
     }
 
-    public double getVitesse(NatureTerrain nature) throws TerrainIncorrectException
-    {
-        switch(nature)
-        {
+    /**
+     * Renvoie la vitesse du {@link Robot} selon le {@link NatureTerrain}.
+     * Le parametre nature doit être non null.
+     * 
+     * @param nature
+     * @return double
+     */
+    public double getVitesse(NatureTerrain nature) {
+        switch (nature) {
             case EAU:
-            /* ne peut pas se déplacer sur l'eau */
+                /* ne peut pas se déplacer sur l'eau */
                 return 0;
             case FORET:
-            /* vitesse diminuée de 50% en forêt */
+                /* vitesse diminuée de 50% en forêt */
                 return this.vitesse * 0.5;
             case ROCHE:
-            /* ne peut pas se déplacer sur la roche */
+                /* ne peut pas se déplacer sur la roche */
                 return 0;
             case TERRAIN_LIBRE:
                 return this.vitesse;
             case HABITAT:
                 return this.vitesse;
             default:
-                /* on throw une exception */
-                throw new TerrainIncorrectException("Le terrain n'est pas correct");
+                // no
+                return Double.NaN;
         }
     }
-    
-    public void deverserEau(int vol) throws VolumeEauIncorrectException
-    {
+
+    public void deverserEau(int vol) throws VolumeEauIncorrectException {
         if (vol < 0) {
             throw new VolumeEauIncorrectException("Le volume d'eau ne peut pas être négatif");
         }
-        /* Si le volume d'eau est supérieur à la capacité du robot, on le met à la capacité max */
+        /*
+         * Si le volume d'eau est supérieur à la capacité du robot, on le met à la
+         * capacité max
+         */
         if (vol > this.volumeEau) {
             vol = this.volumeEau;
         }
@@ -65,13 +74,12 @@ public class Chenilles extends Robot
             this.volumeEau -= vol;
             /* on déverse l'eau sur la position du robot */
             System.out.println("Déversement d'eau sur l'incendie en " + this.position);
-            incendie.decreaseIntensite(vol);             
+            incendie.decreaseIntensite(vol);
         }
     }
 
     @Override
-    public boolean peutRemplir()
-    {
+    public boolean peutRemplir() {
         /* variable si condition respectée ou non */
         boolean peuxRemplir = false;
         /* vérifie si une des cases alentours est de type eau */
@@ -84,18 +92,19 @@ public class Chenilles extends Robot
                     peuxRemplir = true;
                 }
             } catch (IllegalArgumentException e) {
-                /* on ne fait rien, on continue la boucle, pour éviter de raise une erreur si on est sur un bord */
+                /*
+                 * on ne fait rien, on continue la boucle, pour éviter de raise une erreur si on
+                 * est sur un bord
+                 */
             }
         }
         return peuxRemplir;
     }
-    public void remplirReservoir() throws TerrainIncorrectException
-    {
-        if (!this.peutRemplir()) 
-        {
+
+    public void remplirReservoir() throws TerrainIncorrectException {
+        if (!this.peutRemplir()) {
             throw new TerrainIncorrectException("Il n'y a pas d'eau autour du robot");
-        }
-        else {
+        } else {
             this.volumeEau = volumeEauMax;
         }
 
--- tpl_2A_POO/codeEtudiants/src/Robot/Robot.java
+++ tpl_2A_POO/codeEtudiants/src/Robot/Robot.java
@@ -5,30 +5,31 @@ import Simulation.DonneesSimulation;
 import Carte.*;
 import Exception.*;
 
-public abstract class Robot
-{
+public abstract class Robot {
     protected Case position;
     protected int volumeEau;
     protected double vitesse;
     protected TypeRobot type;
     protected DonneesSimulation donnees;
 
-    public Robot(Case position, int volumeEau, double vitesse, DonneesSimulation donnees)
-    {
+    public Robot(Case position, int volumeEau, double vitesse, DonneesSimulation donnees) {
         this.position = position;
         this.volumeEau = volumeEau;
         this.vitesse = vitesse;
         this.donnees = donnees;
     }
 
-    
-    /** 
-     * Renvoie un Robot du {@link TypeRobot} donnée, à la {@link Case} position, 
-     * et avec la vitesse donnée. <p>
-     * Throws: <p>
+    /**
+     * Renvoie un Robot du {@link TypeRobot} donnée, à la {@link Case} position,
+     * et avec la vitesse donnée.
+     * <p>
+     * Throws:
+     * <p>
      * {@link VitesseIncorrectException} si,
-     * pour un type donnée, la vitesse est incorrecte. <p>
+     * pour un type donnée, la vitesse est incorrecte.
+     * <p>
      * {@link NoSuchElementException} si le type du robot est incorrect.
+     * 
      * @param type
      * @param position
      * @param vitesse
@@ -37,138 +38,140 @@ public abstract class Robot
      * @throws NoSuchElementException
      * @throws VitesseIncorrectException
      */
-    public static Robot newRobot(TypeRobot type, Case position, double vitesse, DonneesSimulation donnees) 
-        throws NoSuchElementException, VitesseIncorrectException
-    {
-        switch(type)
-        {
+    public static Robot newRobot(TypeRobot type, Case position, double vitesse, DonneesSimulation donnees)
+            throws NoSuchElementException, VitesseIncorrectException {
+        switch (type) {
             case CHENILLES:
-            /* Volume d'eau temporairement à 0, changé dans le constructeur */
+                /* Volume d'eau temporairement à 0, changé dans le constructeur */
                 return new Chenilles(position, 0, vitesse, donnees);
             case DRONE:
-            //appeller le constructeur du robot drone
+                // appeller le constructeur du robot drone
                 return new Drone(position, 0, vitesse, donnees);
             case ROUES:
-            //appeller le constructeur du robot roues
+                // appeller le constructeur du robot roues
                 return new Roues(position, 0, vitesse, donnees);
             case PATTES:
-            //appeller le constructeur du robot pattes ( pas un copié-collé)
-                return new Pattes(position, donnees);                
+                // appeller le constructeur du robot pattes ( pas un copié-collé)
+                return new Pattes(position, donnees);
             default:
-                //SI TYPE EST NULL CA VA BUG ici
-                //todo
+                // SI TYPE EST NULL CA VA BUG ici
+                // todo
                 throw new NoSuchElementException("Le  type robot " + type.toString() + " n'existe pas!");
         }
     }
 
-    
-    /** 
+    /**
      * Renvoie la position du robot.
+     * 
      * @return Case
      */
     public Case getPosition() {
         return this.position;
     }
-    
-    public TypeRobot getType()
-    {
+
+    public TypeRobot getType() {
         return this.type;
     }
-    /** 
+
+    /**
      * Change la position du Robot.
+     * 
      * @param positionCase
      * @throws TerrainIncorrectException
      */
-    private void setPosition(Case positionCase) throws TerrainIncorrectException
-    {
-        if (this.getVitesse((positionCase).getNature()) == 0)
-        {
+    private void setPosition(Case positionCase) throws TerrainIncorrectException {
+        if (this.getVitesse((positionCase).getNature()) == 0) {
             System.out.println("Le robot ne peut pas se déplacer sur ce terrain");
-        }
-        else
-        {
-            //A changer pue le seum cette partie du code
+        } else {
+            // A changer pue le seum cette partie du code
             this.position = positionCase;
         }
     }
-    
-    
-    /** 
-     * Renvoie true si le Robot est deplacé à la {@link Case} destination, sinon false.
+
+    /**
+     * Renvoie true si le Robot est deplacé à la {@link Case} destination, sinon
+     * false.
+     * 
      * @param destination
      * @return boolean
      */
-    public boolean moveRobot(Case destination)
-    {
-        try
-        {
+    public boolean moveRobot(Case destination) {
+        try {
             this.setPosition(destination);
             return true;
-        } catch (TerrainIncorrectException e)
-        {
+        } catch (TerrainIncorrectException e) {
             return false;
         }
     }
-    
-    /** 
-     * Renvoie true si le Robot est deplacé à la case dans la {@link Direction} dir, sinon false.
+
+    /**
+     * Renvoie true si le Robot est deplacé à la case dans la {@link Direction} dir,
+     * sinon false.
+     * 
      * @param direction
      * @return boolean
      */
-    public boolean moveRobotDirection(Direction direction)
-    {
+    public boolean moveRobotDirection(Direction direction) {
         Carte carte = this.donnees.getCarte();
-        if (carte.voisinExiste(this.position, direction)) return moveRobot(carte.getVoisin(this.position, direction));
-        else return false;
+        if (carte.voisinExiste(this.position, direction))
+            return moveRobot(carte.getVoisin(this.position, direction));
+        else
+            return false;
     }
 
-    
-    /** 
+    /**
      * Renvoie le volume d'eau du Robot.
+     * 
      * @return int
      */
-    public int getVolumeEau()
-    {
+    public int getVolumeEau() {
         return this.volumeEau;
     }
 
-    
-    /** 
+    /**
      * Set le volume d'eau du Robot.
+     * 
      * @param volumeEau
      */
-    public void setVolumeEau(int volumeEau)
-    {
+    public void setVolumeEau(int volumeEau) {
         this.volumeEau = volumeEau;
     }
 
-    
-    /** 
+    /**
      * Renvoie la vitesse du Robot.
+     * 
      * @return double
      */
-    public double getVitesse()
-    {
+    public double getVitesse() {
         return this.vitesse;
     }
 
-    
-    /** 
+    /**
      * Set la vitesse du Robot.
+     * 
      * @param vitesse
      */
-    public void setVitesse(double vitesse)
-    {
+    public void setVitesse(double vitesse) {
         this.vitesse = vitesse;
     }
 
-    //ME TAPEZ PAS 
-    //NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+    // ME TAPEZ PAS
+    // NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     // public DonneesSimulation getDonnees(){
-    //     return this.donnees;
+    // return this.donnees;
     // }
-    public abstract double getVitesse(NatureTerrain nature) throws TerrainIncorrectException;
+    /**
+     * Renvoie la vitesse du {@link Robot} selon le {@link NatureTerrain}.
+     * Le parametre nature doit être non null.
+     * 
+     * @param nature
+     * @return double
+     */
+    public abstract double getVitesse(NatureTerrain nature);
+
     public abstract boolean peutRemplir();
+
     public abstract void deverserEau(int vol) throws VolumeEauIncorrectException;
+
     public abstract void remplirReservoir() throws TerrainIncorrectException;
 }
