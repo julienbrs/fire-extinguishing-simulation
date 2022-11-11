f9c7fa8730de24af14ba3f0631e0077f62ca9022
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2022-11-11 17:47:39 +0100

    new package for shortest path and other strategies

---
+++ tpl_2A_POO/codeEtudiants/src/Strategie/Chemin.java
@@ -0,0 +1,30 @@
+package Strategie;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+import Carte.Case;
+
+public class Chemin {
+    private LinkedList<Case> chemin;
+    private HashMap<Case, Double> couts;
+
+    public Chemin() {
+        this.chemin = new LinkedList<Case>();
+        this.couts = new HashMap<Case, Double>();
+    }
+
+    void add(Case caseToAdd, double date) {
+        chemin.addFirst(caseToAdd);
+        couts.put(caseToAdd, date);
+    }
+
+    public Iterator<Case> getChemin() {
+        return this.chemin.iterator();
+    }
+
+    public double getCout(Case case1) {
+        return this.couts.get(case1);
+    }
+}
---
+++ tpl_2A_POO/codeEtudiants/src/Strategie/Graphe.java
@@ -0,0 +1,103 @@
+package Strategie;
+
+import Carte.*;
+import Robot.Robot;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+/*
+* Graphe où les sommets sont les Case d'une carte donnée.
+* On ne retient pas les aretes parce-que on peut les retrouver
+* avec les voisins dans carte, et la fonction getVitesse du robot.
+*
+*/
+public class Graphe {
+    Carte carte;
+
+    public Graphe(Carte carte) {
+        this.carte = carte;
+    }
+
+    private double Edges(Robot robot, Case source, Case destination) {
+
+        double res = robot.getVitesse(source.getNature()) +
+                robot.getVitesse(destination.getNature());
+        res /= 2;
+        res *= carte.getTailleCases();
+
+        return res;
+    }
+
+    public Chemin Dijkstra(Case source, Case destination, Robot robot) {
+        int nbLignes = this.carte.getNbLignes();
+        int nbColonnes = this.carte.getNbColonnes();
+        int nbElements = nbLignes * nbColonnes;
+        Case prev[][] = new Case[nbLignes][nbColonnes];
+        Chemin chemin = new Chemin();
+
+        CaseComparator comparator = new CaseComparator(nbLignes, nbColonnes);
+
+        PriorityQueue<Case> queue = new PriorityQueue<Case>(nbElements, comparator);
+
+        // initialize prev and ""dist""
+        Case caseCourante = null;
+        for (int ligne = 0; ligne < nbLignes; ligne++) {
+            for (int colonne = 0; colonne < nbColonnes; colonne++) {
+                caseCourante = this.carte.getCase(ligne, colonne);
+                if (caseCourante == source)
+                    continue;
+                comparator.setCout(caseCourante, Double.POSITIVE_INFINITY);
+
+                if (!robot.canMove(caseCourante))
+                    continue;
+
+                queue.add(caseCourante);
+
+                prev[ligne][colonne] = null;
+
+            }
+            comparator.setCout(source, 0);
+            queue.add(source);
+        }
+
+        while (!queue.isEmpty()) {
+            // The main loop
+            // We get the min (head cause min-priority queue)
+            caseCourante = queue.poll();
+
+            if (caseCourante == destination) {
+                Case prec = destination;
+                System.out.println(prev[destination.getLigne()][destination.getColonne()]);
+
+                if (prev[destination.getLigne()][destination.getColonne()] != null || destination == source) {
+                    while (prec != null) {
+                        chemin.add(prec, comparator.getCout(caseCourante));
+                        prec = prev[prec.getLigne()][prec.getColonne()];
+                    }
+                }
+                return chemin;
+            }
+            for (Iterator<Case> voisins = this.carte.getVoisins(caseCourante); voisins.hasNext();) {
+                Case voisin = voisins.next();
+
+                if (!robot.canMove(voisin))
+                    continue;
+                double coutVoisin = comparator.getCout(voisin);
+                double cout = comparator.getCout(caseCourante) + this.Edges(robot, source, destination);
+                if (cout < coutVoisin) {
+                    comparator.setCout(voisin, cout);
+                    prev[voisin.getLigne()][voisin.getColonne()] = caseCourante;
+                    queue.remove(voisin);
+                    queue.add(voisin);
+                }
+
+            }
+        }
+        // On devrait pas arriver la
+        return chemin;
+
+    }
+
+}
