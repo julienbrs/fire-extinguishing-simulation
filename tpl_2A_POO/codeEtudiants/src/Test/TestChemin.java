90455469af2b38a9f75ebd77b84dbf2ab0782a77
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2022-11-12 20:39:05 +0100

    adding testchemin src file

---
+++ tpl_2A_POO/codeEtudiants/src/Test/TestChemin.java
@@ -0,0 +1,52 @@
+package Test;
+
+import Simulation.Simulateur;
+import Strategie.Chemin;
+import Strategie.Graphe;
+import Simulation.DonneesSimulation;
+import Events.*;
+import Robot.*;
+import Carte.*;
+
+import java.io.FileNotFoundException;
+import java.util.zip.DataFormatException;
+
+import java.awt.Color;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+import gui.GUISimulator;
+import gui.Simulable;
+import io.LecteurDonnees;
+
+public class TestChemin {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
+            System.exit(1);
+        }
+
+        try {
+            DonneesSimulation donnees = LecteurDonnees.creeDonneesSimulation(args[0]);
+            int tailleCases = donnees.getCarte().getTailleCases();
+            int nbColonnes = donnees.getCarte().getNbColonnes();
+            int nbLignes = donnees.getCarte().getNbLignes();
+
+            System.out.println(
+                    Integer.toString(tailleCases * nbColonnes) + " " + Integer.toString(tailleCases * nbLignes));
+            GUISimulator gui = new GUISimulator(tailleCases * nbColonnes, tailleCases * nbLignes, Color.lightGray);
+            long time = 0;
+            Simulateur simulateur = new Simulateur(gui, donnees, time);
+            Iterator<Robot> robots = donnees.getRobots();
+            Robot robot = robots.next();
+
+            Graphe g = new Graphe(donnees.getCarte());
+            Chemin chemin = g.Dijkstra(robot.getPosition(), donnees.getCarte().getCase(5, 0), robot);
+            chemin.cheminToEvent(simulateur);
+        } catch (FileNotFoundException e) {
+            System.out.println("fichier " + args[0] + " inconnu ou illisible");
+        } catch (DataFormatException e) {
+            System.out.println("\n\t**format du fichier " + args[0] + " invalide: " + e.getMessage());
+        }
+    }
+}
